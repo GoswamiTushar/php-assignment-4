ed248d4cbe73ade627b6403325d8f6f755d210d9
Tushar Goswami <tushargoswami1510@gmail.com>
2021-09-27 00:37:22 +0530

    Bug fixed

--- script.php
+++ script.php
@@ -23,89 +23,133 @@
 
     <div class="output flex-container ">
         <div class="flex-item">
+
             <?php
-            if (isset($_POST['submit'])) {
-                if ($_POST['md5']) {
-                    echo "<div>MD5 hash: ", md5($_POST['md5']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA for MD5<div>\n";
-                }
+            if ($_POST['md5']) {
+                echo "<div>MD5 hash: ", md5($_POST['md5']), "</div>\n";
+            } else {
+                echo "<div>ENTER DATA for MD5<div>\n";
+            }
+            ?>
 
-                if ($_POST['number-format-1']) {
-                    if ($_POST['number_format-2']) {
-                        echo "<div>Number Format: ", number_format($_POST['number-format-1'], $_POST['number_format-2']), "</div>\n";
-                    } else {
-                        echo "<div>Number Format: ", number_format($_POST['number-format-1']), "</div>\n";
-                    }
+            <?php
+
+            if ($_POST['number-format-1']) {
+                if ($_POST['number_format-2']) {
+                    echo "<div>Number Format: ", number_format($_POST['number-format-1'], $_POST['number_format-2']), "</div>\n";
+                } else {
+                    echo "<div>Number Format: ", number_format($_POST['number-format-1']), "</div>\n";
+                }
+            } else {
+                echo "<div>ENTER DATA</div>\n";
+            }
+            ?>
+
+            <?php
+
+            if ($_POST['ord']) {
+                echo "<div>ASCII of the first character is: ", ord($_POST['ord']), "</div>\n";
+            } else {
+                echo "<div>ENTER DATA for ASCII code</div>\n";
+            }
+
+            ?>
+
+            <?php
+
+            if ($_POST['rtrim-1']) {
+                if ($_POST['rtrim-2']) {
+                    echo "<div>STRING AFTER Right-trim: ", rtrim($_POST['rtrim-1'], $_POST['rtrim-2']), "</div>\n";
+                } else {
+                    echo "<div>STRING AFTER Right-trim: ", rtrim($_POST['rtrim-1']), "</div>\n";
+                }
+            } else {
+                echo "<div>ENTER DATA\n for trimming to right</div>\n";
+            }
+
+            ?>
+
+            <?php
+
+            if ($_POST['strreplace-1']) {
+                if ($_POST['strreplace-2']) {
+                    echo "<div>STRING AFTER REPLACE: ", str_replace($_POST['strreplace-1'], $_POST['strreplace-2'], $_POST['strreplace-3']), "</div>\n";
                 } else {
                     echo "<div>ENTER DATA</div>\n";
                 }
+            }
 
-                if ($_POST['ord']) {
-                    echo "<div>ASCII of the first character is: ", ord($_POST['ord']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA for ASCII code</div>\n";
-                }
+            ?>
 
-                if ($_POST['rtrim-1']) {
-                    if ($_POST['rtrim-2']) {
-                        echo "<div>STRING AFTER Right-trim: ", rtrim($_POST['rtrim-1'], $_POST['rtrim-2']), "</div>\n";
-                    } else {
-                        echo "<div>STRING AFTER Right-trim: ", rtrim($_POST['rtrim-1']), "</div>\n";
-                    }
-                } else {
-                    echo "<div>ENTER DATA\n for trimming to right</div>\n";
-                }
+            <?php
 
-                if ($_POST['strreplace-1']) {
-                    if ($_POST['strreplace-2']) {
-                        echo "<div>STRING AFTER REPLACE: ", str_replace($_POST['strreplace-1'], $_POST['strreplace-2']), "</div>\n";
+            if ($_POST['strlen']) {
+                echo "<div>STRING LENGTH IS: ", strlen($_POST['strlen']), "</div>\n";
+            } else {
+                echo "<div>ENTER DATA for calculating String length</div>\n";
+            }
+
+            ?>
+
+            <?php
+
+            if ($_POST['strcasecmp-1']) {
+                if ($_POST['strcasecmp-2']) {
+                    echo "<div>STRING COMPARISON: ", strcasecmp($_POST['strcasecmp-1'], $_POST['strcasecmp-2']), "</div>\n";
                 } else {
                     echo "<div>ENTER DATA</div>\n";
                 }
+            }
 
-                if ($_POST['strlen']) {
-                    echo "<div>STRING LENGTH IS: ", strlen($_POST['strlen']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA for calculating String length</div>\n";
-                }
+            ?>
 
-                if ($_POST['strcasecmp-1']) {
-                    if ($_POST['strcasecmp-2']) {
-                        echo "<div>STRING COMPARISON: ", strcasecmp($_POST['strcasecmp-1'], $_POST['strcasecmp-2']), "</div>\n";
+            <?php
+            if ($_POST['strrpos-1']) {
+                if ($_POST['strrpos-2']) {
+                    echo "<div>STRING POSITION: ", strrpos($_POST['strrpos-1'], $_POST['strrpos-2']), "</div>\n";
                 } else {
                     echo "<div>ENTER DATA</div>\n";
                 }
-                if ($_POST['strrpos-1']) {
-                    if ($_POST['strrpos-2']) {
-                        echo "<div>STRING POSITION: ", strrpos($_POST['strrpos-1'], $_POST['strrpos-2']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA</div>\n";
-                }
+            }
 
-                if ($_POST['substr-1']) {
-                    echo "<div>STRING AFTER SUBSTRING: ", substr($_POST['substr-1'], $_POST['substr-2'], $_POST['substr-3']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA</div>";
-                }
+            ?>
 
-                if ($_POST['strtolower']) {
-                    echo "<div>ENTERED STRING IN LOWER CASE: ", strtolower($_POST['strtolower']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA for converting string to lower case.\n</div>";
-                }
+            <?php
 
-                if ($_POST['bintohex']) {
-                    echo "<div>ENTERED STRING'S hexadecimal value: ", bin2hex($_POST['bintohex']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA for converting to hexadecimal</div>\n";
-                }
+            if ($_POST['substr-1']) {
+                echo "<div>STRING AFTER SUBSTRING: ", substr($_POST['substr-1'], $_POST['substr-2'], $_POST['substr-3']), "</div>\n";
+            } else {
+                echo "<div>ENTER DATA</div>";
+            }
 
-                if ($_POST['chop-1']) {
-                    echo "<div>STRING AFTER CHOPPING: ", chop($_POST['chop-1'], $_POST['chop-2']), "</div>\n";
-                } else {
-                    echo "<div>ENTER DATA for chopping</div>\n";
-                }
+            ?>
+
+            <?php
+
+            if ($_POST['strtolower']) {
+                echo "<div>ENTERED STRING IN LOWER CASE: ", strtolower($_POST['strtolower']), "</div>\n";
+            } else {
+                echo "<div>ENTER DATA for converting string to lower case.\n</div>";
+            }
+
+            ?>
+
+            <?php
+
+            if ($_POST['bintohex']) {
+                echo "<div>ENTERED STRING'S hexadecimal value: ", bin2hex($_POST['bintohex']), "</div>\n";
+            } else {
+                echo "<div>ENTER DATA for converting to hexadecimal</div>\n";
+            }
+
+            ?>
+
+            <?php
+
+            if ($_POST['chop-1']) {
+                echo "<div>STRING AFTER CHOPPING: ", chop($_POST['chop-1'], $_POST['chop-2']), "</div>\n";
+            } else {
+                echo "<div>ENTER DATA for chopping</div>\n";
             }
             ?>
 
